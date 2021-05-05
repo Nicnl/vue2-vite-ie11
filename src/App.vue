fa03507919f7b10e04d97a059180654bc1a4ce0c
Nicnl <nicnl25@gmail.com>
2021-05-05 14:44:59 +0200

    Super basic vue project with a single route, single component and lazy loading

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        Hello, this is App.vue.<br/>
+        You should see: "this is home page" and a button that counts when clicked.<br/><br/><br/>
+        [BEFORE ROUTER-VIEW]<br/>
+        __________________________________<br/>
+        <br/><br/><router-view/><br/><br/>
+        __________________________________<br/>
+        [AFTER ROUTER-VIEW]<br/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/views/ButtonCounter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button @click="nb++">BUTTON PRESS #{{ nb }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ButtonCounter',
+    data() {
+        return {
+            nb: 0,
+        };
+    },
+}
+</script>
